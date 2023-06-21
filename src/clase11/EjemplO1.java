e745ada2cacab325bbbf48c136e2b667998ad2cd
vitoxint <vitoxint@gmail.com>
2023-06-21 12:13:52 -0400

    ejercicio Ind 20

---
+++ src/clase11/EjemplO1.java
@@ -0,0 +1,29 @@
+package clase11;
+
+public class EjemplO1 {
+
+    public static void main (String args[]) {
+
+        int algo = 5;
+        String var1 = "En un lugar de la Mancha, de cuyo \n ";
+        var1 += "nombre no quiero acordarme, no ha mucho tiempo que vivía un \n";
+        var1 += "hidalgo de los de lanza en astillero, adarga antigua, rocín flaco \n y galgo corredor.\n\n ";
+
+        var1 += "Una olla de algo más vaca que carnero, \n salpicón las más noches, duelos y quebrantos los sábados,\n";
+        var1 += "lantejas los viernes, algún palomino de añadidura los domingos,";
+        var1 +=  "consumían las tres partes de su hacienda.";
+
+/*El resto della concluían sayo de velarte, calzas de
+
+        velludo para las fiestas, con sus pantuflos de lo mesmo,
+y los días de entresemana se honraba con su
+
+                vellorí de lo más fino */
+        int otra_cosa;
+        otra_cosa = 6;
+        System.out.println("El valor de la primera variable es "
+                + algo + " y de la segunda es " + otra_cosa);
+
+        System.out.println("¿Te cuento un cuento?: " + var1);
+    }
+}
